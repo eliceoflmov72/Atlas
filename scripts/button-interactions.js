7d5560ae0940453ab30605c2c2a625859935e8eb
isma <ismaelmartin302@gmail.com>
2024-08-03 21:24:18 +0200

    Wave effect added to button

--- scripts/button-interactions.js
+++ scripts/button-interactions.js
@@ -1,9 +1,15 @@
 export function toggleLightMode() {
     const root = document.documentElement;
+    const expanding_circle = document.getElementById("lightmode-expanding-circle")
     var lightColor = getComputedStyle(root).getPropertyValue('--light');
     var darkColor = getComputedStyle(root).getPropertyValue('--dark');
 
     var colorScheme = getComputedStyle(root).getPropertyValue('--final-background-color');
+
+    expanding_circle.classList.remove("lightmode-expanding-circle-animation");
+    void expanding_circle.offsetWidth;
+    expanding_circle.classList.add("lightmode-expanding-circle-animation");
+
     if (colorScheme === lightColor) {
         root.style.setProperty('--final-background-color', darkColor);
         root.style.setProperty('--initial-background-color', lightColor);
