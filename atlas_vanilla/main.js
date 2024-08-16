a433925c51af407d27f1f093674535a34e3861c1
eliceoflmov72 <eliceomoreta@gmail.com>
2024-08-16 16:25:54 +0200

    npm run lint:fix & new Angular 18 project

--- atlas_vanilla/main.js
+++ atlas_vanilla/main.js
@@ -1,21 +1,21 @@
 import { toggleLightMode } from "./scripts/button-interactions.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const lightModeButton = document.getElementById("lightmode-button");
-    const title = document.querySelector('.animated-title');
-    const letters = title.querySelectorAll('span');
-    
-    lightModeButton.addEventListener("click", () => {
-        toggleLightMode();
-    })
-    letters.forEach((letter, index) => {
-        const angle = (360 / letters.length) * index;
-        const radius = 100; // Puedes ajustar este valor para cambiar la distancia de las letras desde el centro
-        const x = radius * Math.cos(angle * (Math.PI / 180));
-        const y = radius * Math.sin(angle * (Math.PI / 180));
-        
-        letter.style.left = `calc(50% + ${x}px)`;
-        letter.style.top = `calc(50% + ${y}px)`;
-        letter.style.animationDelay = `${index * 0.2}s`; // Ajustar el delay de la animación para cada letra
-    });
+  const lightModeButton = document.getElementById("lightmode-button");
+  const title = document.querySelector(".animated-title");
+  const letters = title.querySelectorAll("span");
+
+  lightModeButton.addEventListener("click", () => {
+    toggleLightMode();
+  });
+  letters.forEach((letter, index) => {
+    const angle = (360 / letters.length) * index;
+    const radius = 100; // Puedes ajustar este valor para cambiar la distancia de las letras desde el centro
+    const x = radius * Math.cos(angle * (Math.PI / 180));
+    const y = radius * Math.sin(angle * (Math.PI / 180));
+
+    letter.style.left = `calc(50% + ${x}px)`;
+    letter.style.top = `calc(50% + ${y}px)`;
+    letter.style.animationDelay = `${index * 0.2}s`; // Ajustar el delay de la animación para cada letra
+  });
 });
--- atlas_vanilla/scripts/button-interactions.js
+++ atlas_vanilla/scripts/button-interactions.js
@@ -1,21 +1,24 @@
 export function toggleLightMode() {
-    const root = document.documentElement;
-    const expanding_circle = document.getElementById("lightmode-expanding-circle")
-    var lightColor = getComputedStyle(root).getPropertyValue('--light');
-    var darkColor = getComputedStyle(root).getPropertyValue('--dark');
+  const root = document.documentElement;
+  const expanding_circle = document.getElementById(
+    "lightmode-expanding-circle"
+  );
+  var lightColor = getComputedStyle(root).getPropertyValue("--light");
+  var darkColor = getComputedStyle(root).getPropertyValue("--dark");
 
-    var colorScheme = getComputedStyle(root).getPropertyValue('--final-background-color');
+  var colorScheme = getComputedStyle(root).getPropertyValue(
+    "--final-background-color"
+  );
 
-    expanding_circle.classList.remove("lightmode-expanding-circle-animation");
-    void expanding_circle.offsetWidth;
-    expanding_circle.classList.add("lightmode-expanding-circle-animation");
+  expanding_circle.classList.remove("lightmode-expanding-circle-animation");
+  void expanding_circle.offsetWidth;
+  expanding_circle.classList.add("lightmode-expanding-circle-animation");
 
-    if (colorScheme === lightColor) {
-        root.style.setProperty('--final-background-color', darkColor);
-        root.style.setProperty('--initial-background-color', lightColor);
-    } else {
-        root.style.setProperty('--final-background-color', lightColor);
-        root.style.setProperty('--initial-background-color', darkColor);
-    }
-
+  if (colorScheme === lightColor) {
+    root.style.setProperty("--final-background-color", darkColor);
+    root.style.setProperty("--initial-background-color", lightColor);
+  } else {
+    root.style.setProperty("--final-background-color", lightColor);
+    root.style.setProperty("--initial-background-color", darkColor);
+  }
 }
