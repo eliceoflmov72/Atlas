524a9e3fa1cd86933758a4115e62f412d16f615f
eliceoflmov72 <eliceomoreta@gmail.com>
2024-08-18 14:16:56 +0200

    New font faces

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,14 @@
-import { Routes } from '@angular/router';
-import { HeaderComponent } from './layout/header/header.component';
+import { provideRouter, Routes } from '@angular/router';
 
-export const routes: Routes = [{ path: '', component: HeaderComponent }];
+import { HomeComponent } from './pages/home/home.component';
+import bootstrap from '../main.server';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home',
+  },
+];
