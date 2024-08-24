ba1ad4374a35d51ad58997d17a24c52f1d1f7f4b
eliceoflmov72 <eliceomoreta@gmail.com>
2024-08-24 12:13:41 +0200

    Footer?

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CardModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
