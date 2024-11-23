f4aef1fa629ead5b9576cdfa5403483e36d47f5d
isma <ismaelmartin302@gmail.com>
2024-11-23 18:42:25 +0100

    "Added app-menu, router-outlet, and app-footer components to app.component.html and imported corresponding components in app.component.ts"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from "./layout/menu/menu.component";
+import { FooterComponent } from "./layout/footer/footer.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [MenuComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
