bd639e12889a08dee8ff00adc829ed2b58925303
isma <ismaelmartin302@gmail.com>
2024-11-23 14:56:53 +0100

    Upgrading to angular 19

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,3 @@
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from './pages/home/home.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
-import { ContactComponent } from './pages/contact/contact.component';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-    pathMatch: 'full',
-    title: 'Inicio',
-  },
-  {
-    path: 'Projects',
-    component: ProjectsComponent,
-    title: 'Proyectos',
-  },
-  {
-    path: 'Contact',
-    component: ContactComponent,
-    title: 'Contacto',
-  },
-];
+export const routes: Routes = [];
