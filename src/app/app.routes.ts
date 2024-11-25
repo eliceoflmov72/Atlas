8d0072bc5ce9d37e60777b6423e75d2c218bd65b
isma <ismaelmartin302@gmail.com>
2024-11-25 15:43:22 +0100

    Added pages path to router outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NewsComponent } from './pages/news/news.component';
+import { TechComponent } from './pages/news/tech/tech.component';
+import { JobsComponent } from './pages/news/jobs/jobs.component';
+import { LearningComponent } from './pages/learning/learning.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'News', component: NewsComponent },
+    { path: 'Tech-news', component: TechComponent },
+    { path: 'Job-news', component: JobsComponent },
+    { path: 'Learning', component: LearningComponent },
+    { path: 'Projects', component: ProjectsComponent },
+    { path: 'Contact', component: ContactComponent },
+    
+    
+    
+    // { path: 'SignIn', component: SigninComponent },
+    // { path: 'SignUp', component: SignupComponent },
+
+];
