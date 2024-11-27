7994a459ed84be33c205665b417eebbdb76f7d02
isma <ismaelmartin302@gmail.com>
2024-11-27 14:46:59 +0100

    Creating the maintenance page and redirect all to this

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,21 +6,25 @@ import { JobsComponent } from './pages/news/jobs/jobs.component';
 import { LearningComponent } from './pages/learning/learning.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { MaintenanceComponent } from './pages/maintenance/maintenance.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'News', component: NewsComponent },
-    { path: 'Tech-news', component: TechComponent },
-    { path: 'Job-news', component: JobsComponent },
-    { path: 'Learning', component: LearningComponent },
-    { path: 'Projects', component: ProjectsComponent },
-    { path: 'Contact', component: ContactComponent },
-    
-    { path: '**', redirectTo: '', pathMatch: "full" }
-    
+    // { path: '', component: HomeComponent },
+    // { path: 'News', component: NewsComponent },
+    // { path: 'Tech-news', component: TechComponent },
+    // { path: 'Job-news', component: JobsComponent },
+    // { path: 'Learning', component: LearningComponent },
+    // { path: 'Projects', component: ProjectsComponent },
+    // { path: 'Contact', component: ContactComponent },
+
+    // { path: '**', redirectTo: '', pathMatch: "full" }
+    { path: 'Maintenance', component: MaintenanceComponent },
+
+
     // { path: 'About-us', component: AboutusComponent },
-    
+
     // { path: 'SignIn', component: SigninComponent },
     // { path: 'SignUp', component: SignupComponent },
 
+    { path: '**', redirectTo: 'Maintenance', pathMatch: "full" },
 ];
