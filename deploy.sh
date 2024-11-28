4039c589b8fc8969fa640487b3b6979d7a628767
isma <ismaelmartin302@gmail.com>
2024-11-28 16:10:13 +0100

    Updated deploy bash

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-# 1. Hacer build del proyecto Angular
 ng build
 
-# 2. Verificar si la carpeta de despliegue existe
 if [ ! -d "dist/zemios/browser" ]; then
   echo "El directorio dist/zemios/browser no existe. Asegúrate de que el build se realizó correctamente."
   exit 1
 fi
 
-# 3. Navegar a la carpeta dist/zemios/browser
 cd dist/zemios/browser
 
-# 4. Verificar si ya hay un repositorio Git
 if [ ! -d ".git" ]; then
   echo "Inicializando el repositorio Git..."
   git init
@@ -21,22 +17,17 @@ else
   echo "Repositorio Git ya inicializado."
 fi
 
-# 5. Traer los últimos cambios del servidor remoto para evitar conflictos de fast-forward
-git pull origin main
-# git fetch origin main
-# git reset --soft origin/main
+git fetch origin main
+git reset --soft origin/main
 
-# 6. Hacer commit y push de los cambios
 git add .
 git commit -m "Actualización del build"
-git push origin main
+git push -u origin main
 
-# 7. Conectarse al servidor para actualizar el sitio en el servidor
-# ssh zemios@zemios.com << 'EOF'
-#   git clone ~/zemios.com.git ~/zemios.com
-#   cd ~/zemios.com
-#   git pull origin main
-# EOF
-
-echo "HA LLEGADO AQUI --------------------------------------------------------------------"
-python 
+ssh zemios@zemios.com << 'EOF'
+  if [ ! -d "/home/zemios/zemios.com" ]; then
+    git clone /home/zemios/zemios.com.git /home/zemios/zemios.com
+  fi
+  cd /home/zemios/zemios.com
+  git pull origin
+EOF
