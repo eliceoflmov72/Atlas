8e01b60d0c26357071dff472f3d4b23312a1cb72
isma <ismaelmartin302@gmail.com>
2024-11-29 12:45:31 +0100

    What we doing cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  joinUsList = [
+    {
+      title: 'Únete a la Comunidad',
+      description: 'Regístrate en nuestra plataforma y crea tu perfil.',
+      icon: 'group.svg',
+    },
+    {
+      title: 'Conecta con Otros Juniors',
+      description: 'Comparte tus experiencias y aprende de otros programadores como tú.',
+      icon: 'chat-bubble-dots.svg',
+    },
+    {
+      title: 'Colabora en Proyectos',
+      description: 'Elige proyectos en los que puedas aportar y que se adapten a tu nivel.',
+      icon: 'git-merge.svg',
+    },
+    {
+      title: 'Sigue Aprendiendo',
+      description: 'Accede a recursos y cursos para seguir desarrollando tus habilidades.',
+      icon: 'chess-pawn.svg',
+    },
+    {
+      title: 'Abre Nuevas Oportunidades',
+      description: 'Haz networking y consigue referencias que te ayuden en tu carrera.',
+      icon: 'key.svg',
+    }
+  ]
 }
