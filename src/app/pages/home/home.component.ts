b9923dcaf516f5eaa1f4c2db2b673b2851337fd5
isma <ismaelmartin302@gmail.com>
2024-12-04 17:57:50 +0100

    Create layout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -41,6 +41,18 @@ export class HomeComponent {
       description: 'Descubre sus nuevas caracteristicas ya mismo.',
       imageURL: 'https://miro.medium.com/v2/resize:fit:1400/1*IKZuN_kzUYPl37lBXQkymQ.png',
       URL: 'https://blog.angular.dev/meet-angular-v19-7b29dfd05b84',
-    }
+    },
+    {
+      title: 'Angular 19 ya esta aquí',
+      description: 'Descubre sus nuevas caracteristicas ya mismo.',
+      imageURL: 'https://miro.medium.com/v2/resize:fit:1400/1*IKZuN_kzUYPl37lBXQkymQ.png',
+      URL: 'https://blog.angular.dev/meet-angular-v19-7b29dfd05b84',
+    },
+    {
+      title: 'Angular 19 ya esta aquí',
+      description: 'Descubre sus nuevas caracteristicas ya mismo.',
+      imageURL: 'https://miro.medium.com/v2/resize:fit:1400/1*IKZuN_kzUYPl37lBXQkymQ.png',
+      URL: 'https://blog.angular.dev/meet-angular-v19-7b29dfd05b84',
+    },
   ]
 }
