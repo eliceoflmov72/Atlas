9831456b05932643da255da4a9a39f3feb637745
isma <ismaelmartin302@gmail.com>
2024-12-05 17:03:52 +0100

    "Updated router links and added URLs to joinUsList items in home component"

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,30 +13,35 @@ export class HomeComponent {
       title: 'Únete a la Comunidad',
       description: 'Regístrate en nuestra plataforma y crea tu perfil.',
       icon: 'group.svg',
+      url: '/SignUp'
     },
     {
       title: 'Conecta con Otros Juniors',
       description: 'Comparte tus experiencias y aprende de otros programadores como tú.',
       icon: 'chat-bubble-dots.svg',
+      url: '/Explore'
     },
     {
       title: 'Colabora en Proyectos',
       description: 'Elige proyectos en los que puedas aportar y que se adapten a tu nivel.',
       icon: 'git-merge.svg',
+      url: '/Projects'
     },
     {
       title: 'Sigue Aprendiendo',
       description: 'Accede a recursos y cursos para seguir desarrollando tus habilidades.',
       icon: 'chess-pawn.svg',
+      url: '/Learning'
     },
     {
       title: 'Abre Nuevas Oportunidades',
       description: 'Haz networking y consigue referencias que te ayuden en tu carrera.',
       icon: 'key.svg',
+      url: '/Explore'
     }
   ];
 
-  mainNews: Array<{title: string, description: string, url: string, imageUrl: string}> = [];
+  mainNews: Array<{ title: string, description: string, url: string, imageUrl: string }> = [];
   lastCourses: Array<{ title: string, description: string, url: string, color: string }> = [];
   lastProyects: Array<{ title: string, description: string, image: string, collaborators: Array<{ image: string, name: string }> }> = [];
 }
