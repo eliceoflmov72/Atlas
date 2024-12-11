3dcc7d1824fcdbc0c19c5a7dcee90777db55a713
isma <ismaelmartin302@gmail.com>
2024-12-11 14:33:32 +0100

    `Refactor projects component template and logic to use Angular's template syntax and a projects array`

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ProjectInterface } from '../../interfaces/project-interface';
 
 @Component({
   selector: 'app-projects',
@@ -7,4 +8,14 @@ import { RouterLink } from '@angular/router';
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+  projects: Array<ProjectInterface> = [{
+    id: 1,
+    name: 'Animalisima',
+    subtitle: 'Una página web sobre animales',
+    description: 'Este es el proyecto 14 de ZEMIOS!!!',
+    image: 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Angular'],
+    startDate: new Date()
+  }]
+}
